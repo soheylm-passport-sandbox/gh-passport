b66d465827b38b66453b37072872c241cadffa1c
SoheylM <41188823+SoheylM@users.noreply.github.com>
2026-09-05 21:08:44 +0200

    feat(release): publish passport curriculum 2.1.2 (#9)

--- cmd/gh-passport/main.go
+++ cmd/gh-passport/main.go
@@ -28,7 +28,7 @@ import (
 var (
 	version           = "0.1.0-dev"
 	controllerAppID   = deployment.ControllerAppID
-	curriculumVersion = "2.1.1"
+	curriculumVersion = "2.1.2"
 )
 
 type doctorCheck struct {
